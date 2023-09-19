f9bd45aa46595fde2247cad18acd07acfdc32fe1
Marco <marco1102004@gmail.com>
2023-09-19 06:40:26 -0600

    Primer commit

---
+++ app/src/main/java/com/example/controlador_android/menuController.java
@@ -0,0 +1,14 @@
+package com.example.controlador_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class menuController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_controller);
+    }
+}
